6967dd76ec895898616023a232620fec8a89d08d
FatmaMejri1 <fatma.mjr11@gmail.com>
2026-03-08 14:48:13 +0100

    add user CRUD controller

---
+++ src/controllers/userController.js
@@ -0,0 +1,70 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+9
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+
+    res.json(users);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.getUserById = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.params.id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+exports.updateUser = async (req, res) => {
+  try {
+
+    const { name, email, role } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      { name, email, role },
+      { new: true }
+    ).select("-password");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json({ message: "User deleted successfully" });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
