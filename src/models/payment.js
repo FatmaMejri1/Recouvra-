dd4754faff4a5d89c2cebc23e973a3f6477e23e7
FatmaMejri1 <fatma.mjr11@gmail.com>
2026-03-09 13:06:22 +0100

    add Payment model with invoice

---
+++ src/models/payment.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema(
+  {
+    invoice: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Invoice",
+      required: true
+    },
+    client: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Client"
+    },
+    amount: {
+      type: Number,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    method: {
+      type: String 
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Payment", paymentSchema);
