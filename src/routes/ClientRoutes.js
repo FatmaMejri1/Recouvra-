d6c05185d42c70a808fe0692c9871aeed605f450
FatmaMejri1 <fatma.mjr11@gmail.com>
2026-03-09 22:33:44 +0100

    Add clientRoutes with auth, role checks, and validation middleware

--- src/routes/ClientRoutes.js
+++ src/routes/ClientRoutes.js
@@ -6,14 +6,14 @@ const clientController = require("../controllers/clientController");
 const auth = require("../middlewares/authMiddleware");
 const authorize = require("../middlewares/roleMiddleware");
 
-router.post("/", auth(), authorize("admin", "manager"), clientController.createClient);
+const validate = require("../middlewares/validate");  
+const { createClientSchema, updateClientSchema } = require("../validators/clientValidator"); 
 
-router.get("/", auth(), authorize("admin", "manager"), clientController.getClients);
 
-router.get("/:id", auth(), authorize("admin", "manager"), clientController.getClientById);
+router.post("/", auth(), authorize("admin", "manager"), validate(createClientSchema), clientController.createClient);  
+router.get("/", auth(), authorize("admin", "manager"), clientController.getClients);  
+router.get("/:id", auth(), authorize("admin", "manager"), clientController.getClientById);  
+router.put("/:id", auth(), authorize("admin", "manager"), validate(updateClientSchema), clientController.updateClient);  
+router.delete("/:id", auth(), authorize("admin"), clientController.deleteClient);  
 
-router.put("/:id", auth(), authorize("admin", "manager"), clientController.updateClient);
-
-router.delete("/:id", auth(), authorize("admin"), clientController.deleteClient);
-
-module.exports = router;
+module.exports = router;  
