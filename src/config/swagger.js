ffd7a8963eeea231290d76120df9d241a537623f
FatmaMejri1 <fatma.mjr11@gmail.com>
2026-03-09 17:34:33 +0100

    Add Swagger API documentation files

---
+++ src/config/swagger.js
@@ -0,0 +1,8 @@
+const YAML = require("yamljs");
+const path = require("path");
+
+const swaggerDocument = YAML.load(
+  path.join(__dirname, "../../docs/swagger.yaml")
+);
+
+module.exports = swaggerDocument; // export the document directly
